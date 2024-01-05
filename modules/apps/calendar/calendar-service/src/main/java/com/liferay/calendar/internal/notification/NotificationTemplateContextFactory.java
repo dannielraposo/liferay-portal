197ee62c2863ba51889d066630e7cffd7ed7c0fd
MarinhoFeliphe <feliphemarinho18@gmail.com>
2024-01-05 11:53:59 -0300

    LPS-205014 SF

--- modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/notification/NotificationTemplateContextFactory.java
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/notification/NotificationTemplateContextFactory.java
@@ -124,7 +124,8 @@ public class NotificationTemplateContextFactory {
 				"location", calendarBooking.getLocation()
 			).put(
 				"portalURL",
-				() -> _getPortalURLOrCompanyDefault(portalURL, user.getCompanyId())
+				() -> _getPortalURLOrCompanyDefault(
+					portalURL, user.getCompanyId())
 			).put(
 				"portletName",
 				LanguageUtil.get(
@@ -210,8 +211,10 @@ public class NotificationTemplateContextFactory {
 			long calendarBookingId, String layoutURL, String portalURL,
 			User user)
 		throws Exception {
+
 		if (layoutURL == null) {
-			GroupLocalService groupLocalService = _groupLocalServiceSnapshot.get();
+			GroupLocalService groupLocalService =
+				_groupLocalServiceSnapshot.get();
 
 			Group group = groupLocalService.getGroup(
 				user.getCompanyId(), GroupConstants.GUEST);
@@ -249,7 +252,7 @@ public class NotificationTemplateContextFactory {
 			String portalURL, long companyId)
 		throws PortalException {
 
-		if (portalURL != null){
+		if (portalURL != null) {
 			return portalURL;
 		}
 
@@ -258,8 +261,7 @@ public class NotificationTemplateContextFactory {
 
 		Company company = companyLocalService.getCompany(companyId);
 
-		GroupLocalService groupLocalService =
-			_groupLocalServiceSnapshot.get();
+		GroupLocalService groupLocalService = _groupLocalServiceSnapshot.get();
 
 		Group group = groupLocalService.getGroup(
 			companyId, GroupConstants.GUEST);
