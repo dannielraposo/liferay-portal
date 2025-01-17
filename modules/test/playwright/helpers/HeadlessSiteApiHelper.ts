4bc6333d5fd7c956c57c92d43836fd1a2aef575d
Jonathan McCann <jonathan.mccann@liferay.com>
2025-01-17 11:55:23 -0800

    LPD-46488 Testing new API

--- modules/test/playwright/helpers/HeadlessSiteApiHelper.ts
+++ modules/test/playwright/helpers/HeadlessSiteApiHelper.ts
@@ -3,19 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {createReadStream} from 'fs';
+import {Site, SiteApi} from '@liferay/headless-site-client-js';
+import {promises} from 'fs';
 
 import {zipFolder} from '../utils/zip';
 import {ApiHelpers} from './ApiHelpers';
-
-type TSite = {
-	externalReferenceCode?: string;
-	id?: number;
-	name: string;
-	parentSiteKey?: string;
-	templateKey?: string;
-	templateType?: string;
-};
 
 export class HeadlessSiteApiHelper {
 	apiHelpers: ApiHelpers;
@@ -26,33 +18,29 @@ export class HeadlessSiteApiHelper {
 		this.basePath = 'headless-site/v1.0';
 	}
 
-	async createSite(site: TSite): Promise<Site> {
-		return this.apiHelpers.post(
-			`${this.apiHelpers.baseUrl}${this.basePath}/sites`,
-			{data: site}
-		);
+	async createSite(site: Site): Promise<Site> {
+		const siteApiClient = await this.apiHelpers.buildRestClient(SiteApi);
+
+		return (await siteApiClient.postSite(site)).body;
 	}
 
 	async createSiteFromZip(
-		site: TSite,
+		site: Site,
 		siteInitializerPath: string
 	): Promise<Site> {
+		const siteApiClient = await this.apiHelpers.buildRestClient(SiteApi);
 		const zip = await zipFolder(siteInitializerPath, {
 			destPath: 'site-initializer/',
 		});
 
-		return this.apiHelpers.post(
-			`${this.apiHelpers.baseUrl}${this.basePath}/sites`,
-			{
-				headers: {
-					...(await this.apiHelpers.getCSRFTokenHeader()),
-				},
-				multipart: {
-					file: createReadStream(zip),
-					site: JSON.stringify(site),
-				},
-			}
-		);
+		const file = new File([await promises.readFile(zip)], 'siteFile');
+
+		return (
+			await siteApiClient.postSiteWithContentType({
+				type: 'multipart/form-data',
+				parameters: {file, site},
+			})
+		).body;
 	}
 
 	async getSiteByERC(externalReferenceCode: string): Promise<Site> {
